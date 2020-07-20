6a0b513d634909d3ed7f4aa65bf9a3dc16b4d409
Veprev Alexey <Alexey.v.veprev@gmail.com>
2020-07-20 12:19:27 +0300

    first commit

---
+++ src/frontend/components/Events/Events.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './CalendarLinks.css';
+
+function CalendarLinks() {
+    return (
+        <section className={styles.mainSection}>
+            <Events />
+        </section>
+    )
+}
+export default CalendarLinks;
